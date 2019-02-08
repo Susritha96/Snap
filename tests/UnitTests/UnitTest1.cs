251d8716c02151ff1388d8c66029aa2173036a2d
Susritha <susrithagade1996@>
2019-02-07 19:54:27 -0600

    commit 3

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,24 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AreaOfCricle()
         {
-
+          var expected = 12.56;
+            var a = 2;
+            var actual = Area(a) ;
+            Assert.Equal(expected,actual); 
+        }
+        double Area(int x)
+        {
+            return 3.14*x*x;  
+        }
+        [Theory]
+        [InlineData(12.56, 2)]
+        [InlineData(78.5, -5 )]
+        [InlineData(0.0, 0 )]
+        public void AreaOfCricle1(double exp, int n)
+        {
+            Assert.Equal(exp, Area(n));
         }
     }
 }
